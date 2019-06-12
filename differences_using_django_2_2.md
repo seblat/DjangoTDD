96872624309fcbcca2792a3537b0e670e9de4a85
Sebastian Latacz <sebastian@serioese.gmbh>
2019-06-12 18:53:29 +0200

    feat(views): introduce a dedicated view_list

--- differences_using_django_2_2.md
+++ differences_using_django_2_2.md
@@ -19,3 +19,20 @@ urlpatterns = [
     path('', views.home_page, name='home')
 ]
 ```
+
+# [Chapter 7.4](https://www.obeythetestinggoat.com/book/chapter_working_incrementally.html#_iterating_towards_the_new_design)
+Commit
+## Original implementation (Django 1.1)
+```
+urlpatterns = [
+    url(r'^$', views.home_page, name='home'),
+    url(r'^lists/the-only-list-in-the-world/$', views.view_list, name='view_list'),
+]
+```
+## Django 2.2 implementation
+```
+urlpatterns = [
+    path('', views.home_page, name='home'),
+    path('lists/the-only-list-in-the-world/', views.view_list, name='view_list'),
+]
+```
--- lists/tests.py
+++ lists/tests.py
@@ -48,3 +48,19 @@ class ItemModelTest(TestCase):
         second_saved_item = saved_items[1]
         self.assertEqual('The first (ever) list item', first_saved_item.text)
         self.assertEqual('Item the second', second_saved_item.text)
+
+
+class ListViewTest(TestCase):
+
+    def test_uses_list_template(self):
+        response = self.client.get('/lists/the-only-list-in-the-world/')
+        self.assertTemplateUsed(response, 'list.html')
+
+    def test_displays_all_items(self):
+        Item.objects.create(text='itemey 1')
+        Item.objects.create(text='itemey 2')
+
+        response = self.client.get('/lists/the-only-list-in-the-world/')
+
+        self.assertContains(response, 'itemey 1')
+        self.assertContains(response, 'itemey 2')
